ce350586a2826015a6810063ae7673232992d313
Minanshu Singh <minanshu.1998@gmail.com>
2019-03-06 20:19:21 +0530

    initial commit

---
+++ precache-manifest.2ba6e880c2fb1301a5b71e48730d72bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5477f54c2263b1428ac5",
+    "url": "/static/css/main.a30e4583.chunk.css"
+  },
+  {
+    "revision": "5477f54c2263b1428ac5",
+    "url": "/static/js/main.5477f54c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "81c89a7b3632153bd3e2",
+    "url": "/static/js/2.81c89a7b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "14bcd322fc17ddbccf75cbe9e311f7b4",
+    "url": "/index.html"
+  }
+];
